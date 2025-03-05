f78f1a1447ce5b9ec18d259c7622e8c5be241cf6
hukasigi <hukasigiteya2@gmail.com>
2025-03-05 14:18:13 +0900

    maybe

--- src/main.cpp
+++ src/main.cpp
@@ -2,55 +2,49 @@
 #include <CAN.h>
 
 const int8_t DEVICE_ID = 0x01; // スレーブのID
-void onreceive(int packetSize)
+void onReceive(int packetSize)
 {
-  if (CAN.parsePacket())
+  if (packetSize == 0)
+    return;
+
+  uint8_t targetID = CAN.read(); // 宛先スレーブID
+  uint8_t command = CAN.read();
+  int8_t receive_data1 = CAN.read();
+  int8_t receive_data2 = CAN.read();
+  int8_t receive_data3 = CAN.read();
+  int8_t receive_data4 = CAN.read();
+
+  // 自分宛のデータか確認
+  if (targetID != DEVICE_ID)
+    return;
+
+  Serial.print("Received Command: ");
+  Serial.println(command, HEX);
+
+  if (command == 0x01)
   {
-    if (packetSize == 0)
-      return; // 受信データがない場合は何もしない
-    uint8_t command;
-    int8_t receive_date1, receive_date2, receive_date3, receive_date4;
-
-    uint8_t senderID = CAN.packetId(); // 送信元のID
-    command = CAN.read();
-    receive_date1 = CAN.read();
-    receive_date2 = CAN.read();
-    receive_date3 = CAN.read();
-    receive_date4 = CAN.read();
-
-    // 自分宛のデータなら処理
-    if (senderID == DEVICE_ID)
-    {
-      Serial.print("Received from Master, Command: ");
-      Serial.println(command, HEX);
-
-      if (command == 0x01)
-      {
-        // モーター制御
-        Serial.print("Motor Control -> l_x: ");
-        Serial.print(receive_date1);
-        Serial.print(" l_y: ");
-        Serial.println(receive_date2);
-        Serial.print(" r_x: ");
-        Serial.println(receive_date3);
-        Serial.print(" r_y: ");
-        Serial.println(receive_date4);
-      }
-      else if (command == 0x02)
-      {
-        // ボタン制御
-        Serial.print("button Control -> circle: ");
-        Serial.print(receive_date1);
-        Serial.print(" triangle: ");
-        Serial.println(receive_date2);
-        Serial.print(" square: ");
-        Serial.println(receive_date3);
-        Serial.print(" cross: ");
-        Serial.println(receive_date4);
-      }
-    }
+    Serial.print("Motor Control -> l_x: ");
+    Serial.print(receive_data1);
+    Serial.print(" l_y: ");
+    Serial.println(receive_data2);
+    Serial.print(" r_x: ");
+    Serial.println(receive_data3);
+    Serial.print(" r_y: ");
+    Serial.println(receive_data4);
   }
-};
+  else if (command == 0x02)
+  {
+    Serial.print("Button Control -> circle: ");
+    Serial.print(receive_data1);
+    Serial.print(" triangle: ");
+    Serial.println(receive_data2);
+    Serial.print(" square: ");
+    Serial.println(receive_data3);
+    Serial.print(" cross: ");
+    Serial.println(receive_data4);
+  }
+}
+
 void setup()
 {
   Serial.begin(115200);
@@ -65,7 +59,7 @@ void setup()
       ;
   }
   Serial.println("Success!");
-  CAN.onReceive(onreceive);
+  CAN.onReceive(onReceive);
 }
 
 void loop()
